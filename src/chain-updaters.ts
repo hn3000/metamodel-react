73fd58281feb1fd6545a0298263cd18170924b5c
Harald Niesche <harald@niesche.de>
2018-09-11 15:12:18 +0200

    only exports in metamodel-react.tsx

---
+++ src/chain-updaters.ts
@@ -0,0 +1,8 @@
+import { IModelView } from '@hn3000/metamodel';
+import { IModelUpdater, IFormContext } from './api';
+
+export function chainUpdaters(...updaters:IModelUpdater[]):IModelUpdater {
+    return (model:IModelView<any>, c:IFormContext) => {
+        return updaters.reduce((m,u) => u(m,c), model);
+    }
+}
--- src/metamodel-react.tsx
+++ src/metamodel-react.tsx
@@ -53,11 +53,6 @@ export {
   MetaContextAwarePure
 } from './base-components';
 
-import { IModelView } from '@hn3000/metamodel';
-import { IModelUpdater, IFormContext } from './api';
-
-export function chainUpdaters(...updaters:IModelUpdater[]):IModelUpdater {
-    return (model:IModelView<any>, c:IFormContext) => {
-        return updaters.reduce((m,u) => u(m,c), model);
-    }
-}
+export {
+  chainUpdaters
+} from './chain-updaters';
